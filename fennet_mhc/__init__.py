94e1b708ca606f985214cba06eeac9557febcb94
jalew188 <jalew188@gmail.com>
2025-05-10 21:49:35 +0800

    reorganize init

--- fennet_mhc/__init__.py
+++ fennet_mhc/__init__.py
@@ -1,16 +1,4 @@
-#!python
-
-import warnings
-
-warnings.filterwarnings("ignore")
-
-__project__ = "fennet-mhc"
 __version__ = "0.0.1"
-__description__ = "Deep learning for multi-dimensional proteomics."
 __github__ = "https://github.com/FennOmix/Fennet.MHC"
-__urls__ = {
-    "GitHub": __github__,
-    # "ReadTheDocs": None,
-    # "PyPi": None,
-    # "Scientific paper": None,
-}
+__doc__ = "https://fennet-mhc.readthedocs.io/en/latest/"
+__license__ = "Apache 2.0"
